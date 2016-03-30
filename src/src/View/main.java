7cdec3ae33493e7f1f3b08b8a3eaf0fac78127ce
x0nePeace <contact@jordanlang.fr>
2016-03-30 21:57:40 +0200

    interface bien avancée

--- src/src/View/main.java
+++ src/src/View/main.java
@@ -1,27 +1,21 @@
 package src.View;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import javax.swing.*;
 
 /**
  * Created by jordan on 22/03/2016.
  */
 public class main {
-    public static src.View.mainFrame frame = new src.View.mainFrame();
+    public static mainFrame frame = new mainFrame();
 
     public static void main(String[] args) {
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("RemoteIP");
         frame.setLocation(300, 200);
-        frame.setVisible(true);
+        frame.setResizable(false);
         frame.pack();
+        frame.setVisible(true);
 
         // System.out.println(System.getProperty("os.name"));
-        // System.out.println(Inet4Address.getLocalHost().getHostAddress());
-
     }
 }
---
+++ src/src/View/serverDialog.java
@@ -0,0 +1,60 @@
+package src.View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by jordan on 30/03/2016.
+ */
+public class serverDialog extends JDialog {
+    private static final String IPADDRESS_PATTERN =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+    private Pattern pattern;
+    private Matcher matcher;
+    private JTextField mdp;
+
+    public serverDialog() {
+        JPanel window = new JPanel();
+        JLabel lbl = new JLabel("Identifiant :");
+        lbl.setHorizontalAlignment(JLabel.CENTER);
+        mdp = new JTextField();
+        mdp.setHorizontalAlignment(JTextField.CENTER);
+        JButton btn_connexion = new JButton("Se connecter");
+        btn_connexion.addActionListener(this::btn_connexion_actionPerformed);
+
+        btn_connexion.setPreferredSize(new Dimension(400, 100));
+
+        GridLayout grid = new GridLayout(3, 1);
+        window.setLayout(grid);
+        window.add(lbl);
+        window.add(mdp);
+        window.add(btn_connexion);
+
+        this.setLayout(new BorderLayout());
+        this.getContentPane().add(window, BorderLayout.CENTER);
+        this.getContentPane().add(Box.createRigidArea(new Dimension(0, 10)), BorderLayout.NORTH);
+        this.getContentPane().add(Box.createRigidArea(new Dimension(0, 10)), BorderLayout.SOUTH);
+        this.getContentPane().add(Box.createRigidArea(new Dimension(10, 0)), BorderLayout.WEST);
+        this.getContentPane().add(Box.createRigidArea(new Dimension(10, 0)), BorderLayout.EAST);
+    }
+
+    public void btn_connexion_actionPerformed(ActionEvent e) {
+        pattern = Pattern.compile(IPADDRESS_PATTERN);
+        matcher = pattern.matcher(getId());
+        if(!matcher.matches()) {
+            JOptionPane.showMessageDialog(this, "Adresse IP non valide", "Erreur", JOptionPane.ERROR_MESSAGE);
+        } else {
+            this.dispose();
+        }
+    }
+
+    public String getId() {
+        return mdp.getText();
+    }
+}
